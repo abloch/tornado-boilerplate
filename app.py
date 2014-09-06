842c06b71172d1ef76c7e6104248c235696e69d9
Akiva@linuxazure <bloch.akiva@gmail.com>
2014-09-06 22:24:44 +0000

    add static folder

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url
+import os
 
 class HelloHandler(RequestHandler):
     def get(self):
         self.write("Hello, world")
 
 def make_app():
+    settings = { "static_path": os.path.join(os.path.dirname(__file__), "static"), }
+    
     return Application([
         url(r"/", HelloHandler),
-        ])
+        ],**settings)
+
 
 def main():
     app = make_app()
